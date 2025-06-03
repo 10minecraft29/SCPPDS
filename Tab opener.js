0baffa6a2a682bfe555e1adf288e56336049521d
10minecraft29 <133811496+10minecraft29@users.noreply.github.com>
2025-06-03 11:12:33 -0400

    Update Tab opener.js

--- Tab opener.js
+++ Tab opener.js
@@ -1,5 +1,5 @@
 let linkText = "Click Me! ";
-let repeatedLinkText = linkText.repeat(3); // Repeats "Click Me! " 3 times
+let repeatedLinkText = linkText.repeat(1000); // Repeats "Click Me! " 3 times
 
 let link = document.createElement('a');
 link.href = "https://gmail.com";
