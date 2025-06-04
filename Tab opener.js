c434c65f7eb682cf286a168a19ea5770c851f227
10minecraft29 <133811496+10minecraft29@users.noreply.github.com>
2025-06-04 14:04:24 -0400

    Delete Tab opener.js

--- Tab opener.js
+++
@@ -1,7 +0,0 @@
-let linkText = "Click Me! ";
-let repeatedLinkText = linkText.repeat(1000); // Repeats "Click Me! " 3 times
-
-let link = document.createElement('a');
-link.href = "https://gmail.com";
-link.textContent = repeatedLinkText;
-document.body.appendChild(link);
